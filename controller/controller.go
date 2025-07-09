c96e2670122a426e8032f9ba9bb6bd668448811e
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2025-07-09 14:47:54 -0300

    criando rota para buscar CPF de aluno, editar informações e deletar aluno

--- controller/controller.go
+++ controller/controller.go
@@ -35,9 +35,46 @@ func GetAlunoByID(c *gin.Context) {
 	var aluno models.Aluno
 	id := c.Params.ByName("id")
 	database.DB.First(&aluno, id)
+	if aluno.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID not found"})
+		return
+	}
 	c.JSON(http.StatusOK, aluno)
 	//if err := database.DB.Where("id = ?", id).First(&aluno).Error; err != nil {
 	//	c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
 	//	return
 	//}
+}
+
+func DeleteAlunos(c *gin.Context) {
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	database.DB.Delete(&aluno, id)
+	c.JSON(http.StatusOK, aluno)
+}
+
+func EditAlunos(c *gin.Context) {
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+
+	database.DB.First(&aluno, id)
+
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	database.DB.Model(&aluno).UpdateColumns(aluno)
+	c.JSON(http.StatusOK, aluno)
+}
+
+func GetAlunoByCpf(c *gin.Context) {
+	var aluno models.Aluno
+	cpf := c.Param("cpf")
+	database.DB.Where(&models.Aluno{CPF: cpf}).First(&aluno)
+	if aluno.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID not found"})
+		return
+	}
+	c.JSON(http.StatusOK, aluno)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -12,6 +12,9 @@ func HandleRequest() {
 	r.GET("/:nome", controller.Hello)
 	r.POST("/alunos", controller.CreateAlunos)
 	r.GET("/alunos/:id", controller.GetAlunoByID)
+	r.DELETE("/alunos/:id", controller.DeleteAlunos)
+	r.PATCH("/alunos/:id", controller.EditAlunos)
+	r.GET("/alunos/cpf/:cpf", controller.GetAlunoByCpf)
 
 	r.Run()
 }
